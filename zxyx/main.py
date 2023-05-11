7e7db293ab16e035758c934d7630672387a49036
rick <103412601+rickhqh@users.noreply.github.com>
2023-05-11 21:39:23 +0800

    Update main.py

--- zxyx/main.py
+++ zxyx/main.py
@@ -1,34 +1,60 @@
-import requests
 import json
-from bs4 import BeautifulSoup
+import requests
+import html
 
-# 构建请求 URL
 url_template = 'http://110.41.160.173:8080/api/student/getQuestions?courseId=ebb2ac4476&page={}&chapterId='
 
-# 遍历所有可能的页码，获取所有题目数据
-questions = []
+with open('1.html', 'w', encoding='utf-8') as f:
+    f.write('<html><head><meta charset="utf-8"></head><body>')
+    simple_question_count = 0
+    judgement_question_count = 0
+    choice_question_count = 0
+    for page in range(1, 10):
+        url = url_template.format(page)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        question_list = data['data']['questionList']
 
-# 全局i
-global i
-i=1
-for page in range(1, 10):
-    url = url_template.format(page)
-    response = requests.get(url)
-    json_data = response.json()
-    questions += json_data['data']['questionList']
-# 解析并输出题目信息
-for idx, question in enumerate(questions):
-    # 去除题目中的 HTML 标签
-    content = BeautifulSoup(question['content'], 'html.parser').get_text()
+        for question in question_list:
+            if question['type'] == '4444':
+                # 简答题
+                answers = question['answer']
+                correct_answer = answers[0]['content']
+                options = ''
+                question_type = '简答题'
+                simple_question_count += 1
+                question_number = '{}'.format(simple_question_count)
+            elif question['type'] == '1111':
+                # 判断题
+                answers = question['answer']
+                correct_answer =answers[0]['content']
+                options = ''
+                question_type = '判断题'
+                judgement_question_count += 1
+                question_number = '{}'.format(judgement_question_count)
+            else:
+                # 选择题
+                answers = question['answer']
+                correct_answer = ''
+                options = ''
+                for j, answer in enumerate(answers):
+                    option = answer['content']
+                    options += '{}<br>'.format( option)
+                    if answer['right']:
+                        correct_answer += chr(ord('A') + j)
+                question_type = '选择题'
+                choice_question_count += 1
+                question_number = '{}'.format(choice_question_count)
 
-    # 去除选项中的 HTML 标签
-    options = [option['content'] for option in question['answer']]
-    answer = options.index([option['content'] for option in question['answer'] if option['right']][0]) + 1
-
-    # 输出第几题
-
-    print(f"### 第{idx+1}题")
-    print(f"题目内容：{question['content'].replace('<p>', '').replace('</p>', '').replace('<br/>', '').replace('<br>', '').replace('<br />', '').replace('&nbsp', '').replace('</p', '').replace('</span>', '')}")
-    for i, option in enumerate(options):
-        print(f"- {option.replace('<p>', '').replace('</p>', '').replace('<br/>', '').replace('<br>', '').replace('<br />', '').replace('&nbsp', '').replace('</p', '').replace('</span>', '')}")
-    print(f"\n\n正确答案 [rick](https://rick.icu) ：{chr(ord('A')+answer-1)}  \n")
+            question_content = question['content'].replace('<p>', '').replace('</p>', '')
+            if question_type in ['简答题']:
+                f.write('<p>## 简答题 第{}题 <br>知识点：{}<br>题目：<p></p>```<p>{}</p>```<br></p>正确答案：<p>```{}</p>```'.format(
+                    question_number, question['point'], question_content, correct_answer))
+            elif question_type in ['判断题']:
+                f.write('<p>## 判断题 第{}题 <br>知识点：{}<br>题目：{}<br></p>正确答案：{}'.format(
+                    question_number, question['point'], question_content, correct_answer))
+            else:
+                f.write('<p>## 选择题 第{}题 <br>知识点：{}<br>题目：{}<br></p>选项：<br><p>```{}</p>```<br><p>正确答案：{}</p>'.format(
+                    question_number, question['point'], question_content, options, correct_answer))
+            f.write('<hr>')
+    f.write('</body></html>')
